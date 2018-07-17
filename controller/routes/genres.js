3c8a89a3d73db547c48fc749ffbbeae0679e8926
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-07-17 18:55:35 +0545

    makes async route handles

--- controller/routes/genres.js
+++ controller/routes/genres.js
@@ -15,60 +15,64 @@ mongoose.connect('mongodb://localhost:27017/vidly', { useNewUrlParser: true })
 //RESTFUL routes
 
 //GET all routes
-router.get('/', (req, res) => {
-    Genre.find()
-        .then(genres => res.json(genres))
+router.get('/', async (req, res) => {
+    const genresfind = await Genre.find()
+    res.json(genresfind)
 })
 
 //POST route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { error } = validationHandler(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
     const newgenre = {
         name: req.body.name
     }
-
-    new Genre(newgenre)
-        .save()
-        .then(genre => res.json(genre))
+    const genrepost = await new Genre(newgenre)
+    const genresave = await genrepost.save();
+    res.json(genresave)
 })
 
 //PUT route
-router.put('/:id', (req, res) => {
-    Genre.findById(req.params.id)
-        .then(genre => {
-            const { error } = validationHandler(req.body)
-            if (error) return res.status(400).send(error.details[0].message)
-            genre.name = req.body.name
-            genre.save()
-                .then(genre => {
-                    res.json(genre)
-                })
-        })
+router.put('/:id', async (req, res) => {
+    try{
+    const genreput = await Genre.findById(req.params.id)
 
-        .catch(err => {
-            return res.status(404).send('No such genre found')
-        })
+    const { error } = validationHandler(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+        genreput.name = req.body.name
+        const editedgenre = await genreput.save()
+
+        res.json(editedgenre)
+
+    }
+    catch(ex){
+        res.status(404).send('No such genre found')
+    }
 
 })
 
+
 //DELETE route
-router.delete('/:id', (req, res) => {
-    Genre.findByIdAndRemove(req.params.id)
-        .then((genre) => {
-            res.json(genre)
-        })
-        .catch(err => res.status(404).send('No such genre found'))
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletegenre = await Genre.findByIdAndRemove(req.params.id)
+        res.json(deletegenre);
+    }
+    catch (err) {
+        res.status(404).send('No such genre found')
+    }
 })
 
 //GET particular route
-router.get('/:id', (req, res) => {
-    Genre.findById(req.params.id)
-        .then((genre) => {
-            res.json(genre)
-        })
-        .catch(err => res.status(404).send('No such genre found'))
+router.get('/:id', async (req, res) => {
+    try{
+    const particulargenre = await Genre.findById(req.params.id)
+        res.json(particulargenre)
+    }
+    catch(err){
+        res.status(404).send('No such genre found')
+    }
 })
 
 function validationHandler(genre) {
